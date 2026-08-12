0196db30ba5651de81a64655d88550229990c735
CloudyClause <bruanjung@gmail.com>
2026-08-12 09:43:46 +0900

    Add files via upload

--- 99_formdatabase/js/script.js
+++ 99_formdatabase/js/script.js
@@ -1,5 +1,4 @@
-const scriptURL = 
-    "https://script.google.com/macros/s/AKfycbwJxzK2Bl7lQr0jbUDC1vqkfjiWsus09mRlzkunuHtrJCSdAR7NswqP16DPdFIbZf9E/exec"; // 자신의 Apps Script 웹 앱 주소 링크 넣는 위치
+const scriptURL = "https://script.google.com/macros/s/AKfycbwJxzK2Bl7lQr0jbUDC1vqkfjiWsus09mRlzkunuHtrJCSdAR7NswqP16DPdFIbZf9E/exec"; // 자신의 Apps Script 웹 앱 주소 링크 넣는 위치
 
 const form = document.forms["submit-to-google-sheet"];
 const msg = document.getElementById("msg");
